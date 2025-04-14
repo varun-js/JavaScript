f08b8a778fad5b390906103590bc9503aa1a9093
varun-js <137644910+varun-js@users.noreply.github.com>
2025-04-14 19:29:40 +0000

    conversion

---
+++ 01_basics/ConversionOperation.js
@@ -0,0 +1,22 @@
+// let Score = "varun";
+
+// console.log(typeof Score);
+// console.log(typeof(Score));
+
+// let valueInNumber = Number(Score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// let isLoggedIn = "varun";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+
+let num = 99;
+
+let strNum = String(num)
+
+console.log(strNum)
+console.log(typeof(strNum))
