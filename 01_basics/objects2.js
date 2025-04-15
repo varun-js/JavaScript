3c06ab46f07f2937da0ae15c7f9a2975e182677d
varun-js <137644910+varun-js@users.noreply.github.com>
2025-04-15 22:11:16 +0000

    object3

--- 01_basics/objects2.js
+++ 01_basics/objects2.js
@@ -53,10 +53,19 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser)
+// console.log(tinderUser)
 
-console.log(Object.keys(tinderUser))
-console.log(Object.values(tinderUser))
-console.log(Object.entries(tinderUser))
+// console.log(Object.keys(tinderUser))
+// console.log(Object.values(tinderUser))
+// console.log(Object.entries(tinderUser))
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'))
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'))
+
+const course = {
+    coursename: "js in kannada",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+const {Instructor} = course
+console.log(Instructor) 
