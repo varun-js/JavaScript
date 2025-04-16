0d6353a5d50d8bd93c0ca19c736adebd09f06747
varun-js <137644910+varun-js@users.noreply.github.com>
2025-04-16 16:28:50 +0000

    IIFE

---
+++ 01_basics/IIFE.js
@@ -0,0 +1,12 @@
+//Immediately Invoked Function Expression (IIFE)
+
+//named IIFE
+(function chai(){
+    console.log(`db connected`);
+})();//madatary ; to run next fun
+//avoid the pollution of global scope
+
+//unamed IIFE
+( (name) =>{
+    console.log(`db connected Tow ${name}`)
+})("Varun");
