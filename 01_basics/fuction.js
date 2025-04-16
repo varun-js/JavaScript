94ad215a68cc85850a016a3b2adffd7651c03bca
varun-js <137644910+varun-js@users.noreply.github.com>
2025-04-16 14:28:14 +0000

    function

---
+++ 01_basics/fuction.js
@@ -0,0 +1,28 @@
+function sayMyName(){
+    console.log("Varun");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(n1, n2){
+//    console.log(n1 + n2);
+
+// }
+// function addTwoNumbers(n1, n2){
+//     return n1 + n2;
+// }
+
+// const result = addTwoNumbers(1, 2)
+// console.log(result);
+
+
+const arr = [200, 300, 400]
+
+function returnArrValue(array){
+    return array[2]
+}
+
+console.log(returnArrValue)
+
+
+
